51d11ffbe657227b9741b97e8d8ee1fa268eded5
IslombekOrifov <islombekorifov0199@gmail.com>
2023-10-30 18:19:05 +0500

    generating pdf file for order

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,7 @@
 import csv
 import datetime
 
+from django.urls import reverse
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 from django.http import HttpResponse
@@ -42,11 +43,24 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+order_pdf.short_description = "Invoice"
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    order_stripe_payment, 'created', 'updated']
+                    order_stripe_payment, 'created', 'updated',
+                    order_detail, order_pdf
+                    ]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
