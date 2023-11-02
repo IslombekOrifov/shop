413f564b3dfd5b10b421dec7dfd25c1bb7d76b5a
IslombekOrifov <islombekorifov0199@gmail.com>
2023-11-03 01:25:04 +0500

    coupon system integration done

--- payment/tasks.py
+++ payment/tasks.py
@@ -18,7 +18,7 @@ def payment_completed(order_id):
                          message,
                          'islombekorifov0199@mail.com',
                          [order.email])
-    html = render_to_string('orders/order/pdf.html', args=[order.id])
+    html = render_to_string('orders/order/pdf.html', {'order': order})
     out = BytesIO()
     stylesheets = [weasyprint.CSS(settings.STATIC_ROOT / 'css/pdf.css')]
     weasyprint.HTML(string=html).write_pdf(out, stylesheets)
